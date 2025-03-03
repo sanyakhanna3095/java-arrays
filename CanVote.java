2a28ccf0f91beee5ede8ec9bf9388be99c6649d3
sanyakhanna3095 <sanya1266.be21@chitkara.edu.in>
2025-03-03 13:46:03 +0530

    [Sanya] Add: According to age of each student, determine if they can vote or not

--- CanVote.java
+++ CanVote.java
@@ -5,9 +5,14 @@ public class CanVote{
 		Scanner sc = new Scanner(System.in);
 		//Create and Initialise an array
 		int[] ages=new int[10];
+
+		//Add elements to array using for loop
 		for(int i=0;i<10;i++){
 			ages[i]=sc.nextInt();
 		}
+		
+
+		//Determine if students can perform or not
 		for(int i=0;i<ages.length;i++){
 			if(ages[i]>=18){
 				System.out.println("The student with the age " + ages[i] + " can vote");
